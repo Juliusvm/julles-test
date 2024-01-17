5fd70bbc20d961dda08f9662c1d2465bbda983de
Julius van Mansvelt <julius.van-mansvelt@mer.eco>
2024-01-17 21:42:49 +0100

    init

---
+++ JullesApp/src/main/java/com/example/jullesapp/JullesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.jullesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JullesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JullesAppApplication.class, args);
+	}
+
+}
---
+++ JullesApp/src/test/java/com/example/jullesapp/JullesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jullesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JullesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
